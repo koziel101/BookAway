109f888e4f7dc9a3ee52989bb1aa1cdfe82a6f76
MarjorieCelestino <marjorie_celestino@hotmail.com>
2016-06-29 00:12:45 -0300

    Criação do Projeto
    Criação do projeto no AndroidStudio.

---
+++ Mobile/Código Fonte/BookAway/app/src/main/java/com/example/android/bookaway/selected_book.java
@@ -0,0 +1,13 @@
+package com.example.android.bookaway;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class selected_book extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selected_book);
+    }
+}
