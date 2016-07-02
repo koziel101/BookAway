a0e2a56b8152f21a91364b1dcc8c80be809628cd
Leonardo Costa <LordKalyn@hotmail.com.br>
2016-07-02 19:32:12 -0300

    Inserido Tela de Pesquisa (inicial)
    Obs: a tela não está pronta

---
+++ Mobile/BookAway/app/src/main/java/com/example/android/bookaway/SearchActivity.java
@@ -0,0 +1,28 @@
+package com.example.android.bookaway;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+public class SearchActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+        mRecyclerView = (RecyclerView) findViewById(R.id.search_recycle_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+    }
+}
