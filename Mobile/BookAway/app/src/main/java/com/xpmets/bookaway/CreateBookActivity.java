d8bcf174e7ca354195f14428fd5e63467deee001
koziel101 <hiagokoziel100@gmail.com>
2016-07-15 21:38:26 -0300

    Criando as Activity de adicionar livro

---
+++ Mobile/BookAway/app/src/main/java/com/xpmets/bookaway/CreateBookActivity.java
@@ -0,0 +1,16 @@
+package com.xpmets.bookaway;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Hiago on 15/07/2016.
+ */
+public class CreateBookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_create_book);
+    }
+}
