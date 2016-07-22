fa64bf8f156e03a9baea1166db01b5606a514df4
MarjorieCelestino <marjorie_celestino@hotmail.com>
2016-07-22 02:15:30 -0300

    Adicionado código das páginas e redirecionamento

---
+++ Mobile/Ionic/bookAway/js/controllers.js
@@ -0,0 +1,38 @@
+angular.module('app.controllers', [])
+  
+.controller('bookAwayCtrl', function($scope) {
+
+})
+   
+.controller('estanteCtrl', function($scope) {
+
+})
+      
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('signupCtrl', function($scope) {
+
+})
+   
+.controller('meuPerfilCtrl', function($scope) {
+
+})
+   
+.controller('livroCtrl', function($scope) {
+
+})
+   
+.controller('visualizaLivroCtrl', function($scope) {
+
+})
+   
+.controller('novoLivroCtrl', function($scope) {
+
+})
+   
+.controller('favoritosCtrl', function($scope) {
+
+})
+ 
